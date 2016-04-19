dac205a2bc3ccc9bf57c9819c757a4a3dee1e2c8
idriyslowe <idriyswins@gmail.com>
2016-04-19 17:40:33 -0500

    saving progress fixing recursion bug in add query method

--- output_easy.rb
+++ output_easy.rb
@@ -11,18 +11,22 @@ class ReadFile
 end
 
 class TreeOutput
+	attr_accessor :instructions_array, :nodes_array
 	@input_problem_array = ReadFile.read_input("/Users/igrant/CancerIQ/samples/input01.txt")
 	@nodes_array = []
 	@instructions_array = []
 	@unique_nodes_hash = {}
 	@path_hash = {}
+	@final_path = []
 
 	def self.output
 		split_lines
 		make_nodes_hash
 		find_common_node
+		end_nodes
 		read_instructions
 	end
+	# CHECKS OUT!!! CREATES NODES AND INSTRUCTIONS ARRAY
 	def self.split_lines
 		@input_problem_array.each do |string|
 			new_array = string.split
@@ -34,14 +38,14 @@ class TreeOutput
 		end
 		@instructions_array << @nodes_array.pop
 	end
-
+	# CHECKS OUT!!! INITIALIZES HASH WITH ALL NODES VALUE 0
 	def self.make_nodes_hash
 		new_array = @nodes_array.flatten.uniq
 		new_array.each do |node|
 			@unique_nodes_hash[node] = 0
 		end
 	end
-
+	# CHECKS OUT!!! MAKES HASH WITH NODE AND EVERY ONE IT TOUCHES
 	def self.find_common_node
 		@unique_nodes_hash.each_key do |node|
 			new_array = @nodes_array.select { |array| array.include?(node) }
@@ -50,28 +54,64 @@ class TreeOutput
 			@path_hash[node] = node_and_relatives
 		end
 	end
-
+	# CHECKS OUT!!! CALLS MAX AND ADD WHEN INSTRUCTED
 	def self.read_instructions
-		@instructions_array.each do |array|
-			add_query(array) if array[0] == "add"
-			max_query(array) if array[0] == "max"
+		@instructions_array.each do |query_array|
+			add_query(query_array) if query_array[0] == "add"
+			max_query(query_array) if query_array[0] == "max"
 		end
 	end
+	# DOUBLE CHECK THAT NEGATIVE VALUES ARE ADDED TO ALL IN THE CASE OF 1
 	def self.add_query(array)
-		@unique_nodes_hash[array[1]] += array[2].to_i
-		node_relatives = @path_hash.select { |node, relatives| relatives.include?(array[1]) }
-		if node_relatives.length > 1
-			node_relatives.each do |node, relatives|
-				if relatives.length == 1
-					@unique_nodes_hash[node] += array[2].to_i
-				else
-					relatives.each do |node|
-						@unique_nodes_hash[node] += array[2].to_i if @path_hash[node].include?(array[1])
-					end
-				end	
+		# leave final path here so it only deletes when you call it
+		@final_path = []
+		if array[1] == "1"
+			@unique_nodes_hash.each { |node, value| value += array[2].to_i }
+		else
+			@unique_nodes_hash[array[1]] += array[2].to_i
+			final_path = find_path_to_end(array[1])
+		end
+	end
+	# DETERMINE END NODES! ONLY TOUCH ONE OTHER NODE. ITS CLOSEST PARENT DOESN'T HAVE CHILDREN
+	def self.end_nodes
+		@end_nodes = @path_hash.select { |node, relatives| relatives.length == 1 && node.to_i > 1 }
+	end
+	# START NODE IS ALREADY AFFECTED ABOVE. DONT FORGET TO KEEP STARTING RELATIVES!
+	# def self.find_path_to_end(start_node)
+	# 	puts "Start node #{start_node}"
+	# 	# !!start_node = 2
+	# 	@final_path = []		
+	# 	starting_relatives = @path_hash.select { |node, relatives| relatives.include?(start_node) && node.to_i > start_node.to_i }
+	# 	# !!starting_relatives =  {"4"=>["2", "8"], "5"=>["2"]}
+	# 	@final_path << starting_relatives.keys
+	# 	starting_relatives.values.each do |relatives|
+	# 	# !!relatives = ["2", "8"] ["2"]
+	# 		relatives.each do |relative|
+	# 			# !!relative = "2", "8", "2" ignoring all twos below
+	# 			@final_path << @path_hash[relative] unless relative == start_node
+	# 			# final_path = [["4", "5"], ["4", "9"]]
+	# 			puts "relative #{relative} and it's values #{@path_hash[relative]}. Final path\n#{@final_path.inspect}"
+	# 		end
+	# 	end
+	# 	# continue retrieving node values until you run out
+	# end
+
+	def self.find_path_to_end(start_node)
+		last_crawl = []		
+		starting_relatives = @path_hash.select { |node, relatives| relatives.include?(start_node) && node.to_i > start_node.to_i }
+		@final_path << starting_relatives.keys
+		starting_relatives.values.each do |relatives|
+			relatives.each do |relative|
+				last_crawl << @path_hash[relative] unless relative == start_node
+				@final_path << @path_hash[relative] unless relative == start_node
 			end
 		end
-	end
+		last_crawl.flatten!
+		last_crawl.each do |node|
+			find_path_to_end(node)
+		end
+		full_path = @final_path.flatten
+	end	
 
 	def self.sub_path(sub_array)
 		temp_array = []
@@ -82,6 +122,7 @@ class TreeOutput
 		temp_array
 	end
 	
+	# CAN WE COMBINE THIS WITH MAKE_PATHS
 	def self.max_query(array)
 		make_paths(array)
 	end
@@ -106,61 +147,10 @@ class TreeOutput
 
 	def self.calculate_max(ultimate_path)
 		values = @unique_nodes_hash.select { |node, value| ultimate_path.include?(node) }
-		max = values.values.max_by { |value| value}
-		puts max
+		max = values.values.max_by { |value| value }
+		# puts max
 	end
 end
 
-# ReadFile.read_input("/Users/igrant/CancerIQ/samples/input00.txt")
-
 TreeOutput.output
 
-# Create graph
-# 1. read file line by line, ignoring empty lines OR take input as an array of an array of string for now like the file read would produce
-# 2. Split and comma separated arrays: DONE
-# 3. Process arrays differently based on their length: DONE
-# 	Node lines length == 2
-# 	Instructions length == 3
-#   The Node that is last OR not found in any other node array is the query quantity number:
-# 4. Make hash (default 0) of flattened array for output: DONE
-	# Flatten array and pull out all uniques
-# 5. Make path arrays out of all paths by joining arrays with common numbers, omit non-unique numbers:
-	# Make array initiating w/end nodes
-	# Add the arrays from those nodeses path hashes
-	# CHECK for any duplicate nodes
-	# If not, retrieve relative nodes arrays and check again
-	# For each key's flatten, remove unique, shovel into path array
-
-# Create output
-# 1. Iterate through split array reading instructions
-	# If begins with "add", add second number to value in hash.
-	# Find nodes that are only related to one other node
-	# OR
-	# Find nodes child nodes (in other hashes & others are in its hash)
-	# If begins with "max", read values of all nodes in path from hash to find highest value. Output node.
-	# 	For each node, save and read value from the hash.
-	# 	Calculate your max from the saved values.
-	# 	Output that
-
-# add occurs for itself & all child nodes for non-end nodes
-# max is looking for max value of nodes on path from a to b
-# ONLY OUTPUTTING MAX VALUES. adding is a quiet step
-
-# input00 easy
-# 5
-# 1 2
-# 2 3
-# 2 4
-# 5 1
-# 6
-# add 4 30
-# add 5 20
-# max 4 5
-# add 2 -20
-# max 4 5
-# max 3 4
-
-# output00 easy
-# 30
-# 20
-# 10
