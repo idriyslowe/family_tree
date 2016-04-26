404b54a940d529b49b7068a4dffca43daf3e3ebd
idriyslowe <idriyswins@gmail.com>
2016-04-26 14:48:58 -0500

    rewritten again but easy 01 and 02 both work

--- output_easy.rb
+++ output_easy.rb
@@ -10,109 +10,170 @@ class ReadFile
 	end
 end
 
-class TreeOutput
-	@input_problem_array = ReadFile.read_input("/Users/igrant/CancerIQ/samples/test_samples.txt")
+require 'set'
+
+class Node
+	attr_accessor :name, :relatives, :value
+	def initialize(name)
+		@name = name
+		@relatives = Set.new
+		@value = 0
+	end
+end
+
+class Graph
+	@graph = ReadFile.read_input("/Users/igrant/family_tree/samples/input02.txt")
 	@nodes_arrays = []
-	@instructions_array = []
-	@unique_nodes_hash = {}
+	@queries_array = []
+	@node_collection = []
 
 	def self.output
 		split_lines
-		make_nodes_hash
-		read_instructions
+		make_node_collection
+		update_node_relatives
+		read_queries
 	end
-
 	def self.split_lines
-		@input_problem_array.each do |string|
+		@graph.each do |string|
 			new_array = string.split
 			if new_array.length < 3
 				@nodes_arrays << new_array
 			else
-				@instructions_array << new_array
+				@queries_array << new_array
 			end
 		end
-		@instructions_array << @nodes_arrays.pop
+		# WHY ISN'T THE POP PUSH WORKING?!
+		@queries_array.push(@nodes_arrays.pop)
 		@nodes_total = @nodes_arrays.shift
+		# puts "Last in nodes array #{@nodes_arrays.pop}"
+		# puts "Queries array #{@queries_array.inspect}"
 	end
-
-	def self.make_nodes_hash
-		new_array = @nodes_arrays.flatten.uniq
-		new_array.each do |node|
-			@unique_nodes_hash[node] = 0
+	def self.find_relatives(node)
+		@nodes_arrays.select { |array| array.include?(node) }
+	end
+	def self.make_node_collection
+		unique_nodes = @nodes_arrays.flatten.uniq
+		unique_nodes.each do |node|
+			current_node = Node.new(node)
+			@node_collection << current_node
 		end
 	end
-
-	def self.end_node?(node)
-		node_count = @nodes_arrays.select { |array| array.include?(node) }
-		return true if node_count.length == 1
+	def self.update_node_relatives
+		@node_collection.each do |node|
+			relatives = find_relatives(node.name).flatten.uniq
+			relatives.delete(node)
+			relatives.each { |relative| node.relatives << find_node(relative) }		
+		end
 	end
-
-	def self.read_instructions
-		@instructions_array.each do |query_array|
+	def self.end_node?(node)
+		relatives = find_relatives(node)
+		return true if relatives.length == 1
+	end
+	def self.read_queries
+		@queries_array.each do |query_array|
 			add_query(query_array) if query_array[0] == "add"
 			max_query(query_array) if query_array[0] == "max"
 		end
 	end
+	def self.find_node(name)
+		@node_collection.find {|node| node.name == name }
+	end
 	def self.add_query(array)
+		# puts "ADD QUERY #{array[1]}"
 		if array[1] == "1"
-			@unique_nodes_hash.each do |node, value|
-				@unique_nodes_hash[node] += array[2].to_i
+			@node_collection.each do |node|
+				node.value += array[2].to_i
 			end
 		elsif end_node?(array[1])
-			@unique_nodes_hash[array[1]] += array[2].to_i
+			node = find_node(array[1])
+			node.value += array[2].to_i
 		else
-			@unique_nodes_hash[array[1]] += array[2].to_i
-			family_tree = crawl_tree(array[1])
+			family_tree = []
+			root = find_node("1")
+			node = find_node(array[1])
+			tree_search = BreadthSearch.new(node)
+			avoid = tree_search.shortest_path_to(root)
+			ugh_search = tree_search.find_descendants(node, avoid)
+			family_tree << ugh_search
+			family_tree.flatten!
 			family_tree.each do |node|
-				@unique_nodes_hash[node] += array[2].to_i
+				node.value += array[2].to_i
 			end
 		end
 	end
-	def self.crawl_tree(start_node)
-		family_tree = []
-		immediate_relatives = @nodes_arrays.select { |array| array.include?(start_node) }.flatten!
-		immediate_relatives.delete_if { |node| node.to_i <= start_node.to_i }
-		family_tree << immediate_relatives
-		family_tree << loop_through_relatives(immediate_relatives)
-		family_tree.flatten
-	end
-
-	def self.loop_through_relatives(node_array, depth = 0, final_nodes = [])
-		node_array.each do |current_node|
-			unless end_node?(current_node) || depth >= 9
-				depth += 1				
-				cousins = @nodes_arrays.select { |array| array.include?(current_node) }.flatten!
-				cousins.delete_if { |node| node.to_i <= current_node.to_i } unless cousins == nil
-				loop_through_relatives(cousins, depth + 1, final_nodes.push(cousins))
-			end
-		end
-		final_nodes
-	end
-
 	def self.max_query(array)
-		full_path = []
-		higher_node = [array[1].to_i, array[2].to_i].max.to_s
-		lower_node = [array[1].to_i, array[2].to_i].min.to_s
-		full_path << lower_node
-		full_path << higher_node
-		full_path << higher_node_crawl(lower_node, higher_node)
-		full_path << higher_node_crawl(higher_node, lower_node)
-		puts "Full path #{full_path}"
-		# if there's a duplicate in a number other than 1, get rid of it. its extra
-	end
-
-	def self.higher_node_crawl(start_node, other_node, full_path = [])
-		if !full_path.include?(start_node) || !full_path.include?("1")
-			first_low_neighbor = @nodes_arrays.select { |array| array.include?(other_node) }.flatten!
-			first_low_neighbor.delete_if { |node| node.to_i >= other_node.to_i } if first_low_neighbor != nil
-			higher_node_crawl(start_node, first_low_neighbor[0], full_path.push(first_low_neighbor)) if first_low_neighbor != nil
+		values = []
+		source_node = find_node(array[1])
+		terminal_node = find_node(array[2])
+		search = BreadthSearch.new(source_node)
+		family_tree = search.shortest_path_to(terminal_node)
+		family_tree.each do |node|
+			values << node.value
 		end
-		puts "Start node #{start_node} Other node #{other_node} Full path\n#{full_path}"
-		full_path
+		puts values.max
 	end
-
-	def self.
 end
 
-TreeOutput.output
+class BreadthSearch
+	attr_accessor :visited
+	def initialize(source_node)
+		@source_node = source_node
+		@visited = []
+		@edge_to = {}
+
+		breadth_first_search(source_node)
+	end
+	def shortest_path_to(node)
+    return unless has_path_to?(node)
+    path = []
+
+    while(node != @source_node) do
+      path.unshift(node)
+      node = @edge_to[node]
+    end
+
+    path.unshift(@source_node)
+	end
+	def find_descendants(start_node, parents_array)
+		# visited collects all
+		visited = [start_node]
+		# queue holds what we're searching
+		queue = []
+		queue << start_node
+		# find descendants of node 2
+		while queue.any?
+			current_node = queue.shift
+			current_node.relatives.each do |relative|
+				next if visited.include?(relative) || parents_array.include?(relative)
+				# relative 1, 2, 4, 5
+				# add relatives to path unless it's in the parents_array
+				queue << relative
+				visited << relative
+				current_node = relative
+			end
+		end
+		visited
+	end
+	private
+	def breadth_first_search(node)
+    queue = []
+    queue << node
+    @visited << node
+
+    while queue.any?
+	    current_node = queue.shift
+	    current_node.relatives.each do |adjacent_node|
+	      next if @visited.include?(adjacent_node)
+	      queue << adjacent_node
+	      @visited << adjacent_node
+	      @edge_to[adjacent_node] = current_node
+	    end
+	  end
+	end
+	def has_path_to?(node)
+	    @visited.include?(node)
+	end
+end
+
+Graph.output
 
